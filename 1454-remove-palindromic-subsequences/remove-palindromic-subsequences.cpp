8c2013b945847af8b5ed1ae74e2765f487cc0549
Aastha W <waastha4@gmail.com>
2026-07-23 23:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (40.24%) - LeetSync

---
+++ 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int n=s.size();
+        if(n==1) return 1;
+        for(int i=0;i<n;i++){
+            if(s[i]!=s[n-1-i]) return 2;
+        }
+        return 1;
+    }
+};
